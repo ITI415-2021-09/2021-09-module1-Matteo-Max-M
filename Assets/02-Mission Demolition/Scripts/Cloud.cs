a421d7bb1d725fce1417d32128b7e7f844cd3dec
Matthew M <matteomax98@gmail.com>
2021-11-19 16:21:03 -0500

    Mission Demolition Pre-Final Build - Debugging needed

--- Assets/02-Mission Demolition/Scripts/Cloud.cs
+++ Assets/02-Mission Demolition/Scripts/Cloud.cs
@@ -4,65 +4,69 @@ using UnityEngine;
 
 public class Cloud : MonoBehaviour
 {
-	[Header("Set in Inspector")]
-	public GameObject 	cloudSphere;
-	public int 	numSpheresMin = 6;
-	public int 	numSpheresMax = 10;
-	public Vector3 	sphereOffsetScale = new Vector3(5,2,1);
-	public Vector2 	sphereScaleRangeX = new Vector2(4,8);
-	public Vector2 	sphereScaleRangeY = new Vector2(3,4);
-	public Vector2 	sphereScaleRangeZ = new Vector2(2,4);
-	public float 	scaleYMin = 2f;
+    [Header("Set in Inspector")]                                                       // a
+    public GameObject cloudSphere;
+    public int numSpheresMin = 6;
+    public int numSpheresMax = 10;
+    public Vector3 sphereOffsetScale = new Vector3(5, 2, 1);
+    public Vector2 sphereScaleRangeX = new Vector2(4, 8);
+    public Vector2 sphereScaleRangeY = new Vector2(3, 4);
+    public Vector2 sphereScaleRangeZ = new Vector2(2, 4);
+    public float scaleYMin = 2f;
 
-	private List<GameObject> 	spheres;
+    private List<GameObject> spheres;                                              // b
 
-    // Start is called before the first frame update
     void Start()
     {
         spheres = new List<GameObject>();
 
-        int num = Random.Range(numSpheresMin, numSpheresMax);
-        for(int i = 0; i<num; i++) {
-        	GameObject sp = Instantiate<GameObject>(cloudSphere);
-        	spheres.Add(sp);
-        	Transform spTrans = sp.transform;
-        	spTrans.SetParent(this.transform);
+        int num = Random.Range(numSpheresMin, numSpheresMax);                          // c
+        for (int i = 0; i < num; i++)
+        {
+            GameObject sp = Instantiate<GameObject>(cloudSphere);                    // d
+            spheres.Add(sp);
+            Transform spTrans = sp.transform;
+            spTrans.SetParent(this.transform);
 
-        	// randomly assign a position
-        	Vector3 offset = Random.insideUnitSphere;
-        	offset.x *= sphereOffsetScale.x;
-        	offset.y *= sphereOffsetScale.y;
-        	offset.z *= sphereOffsetScale.z;
-        	spTrans.localPosition = offset;
+            // Randomly assign a position
+            Vector3 offset = Random.insideUnitSphere;                                  // e
+            offset.x *= sphereOffsetScale.x;
+            offset.y *= sphereOffsetScale.y;
+            offset.z *= sphereOffsetScale.z;
+            spTrans.localPosition = offset;                                            // f
 
-        	// randomly assign scale
-        	Vector3 scale = Vector3.one;
-        	scale.x = Random.Range(sphereScaleRangeX.x, sphereScaleRangeX.y);
-        	scale.y = Random.Range(sphereScaleRangeY.x, sphereScaleRangeY.y);
-        	scale.z = Random.Range(sphereScaleRangeZ.x, sphereScaleRangeZ.y);
+            // Randomly assign scale
+            Vector3 scale = Vector3.one;                                              // g
+            scale.x = Random.Range(sphereScaleRangeX.x, sphereScaleRangeX.y);
+            scale.y = Random.Range(sphereScaleRangeY.x, sphereScaleRangeY.y);
+            scale.z = Random.Range(sphereScaleRangeZ.x, sphereScaleRangeZ.y);
 
-        	//adjust y scale by x distance from core
-        	scale.y *= 1 - (Mathf.Abs(offset.x) / sphereOffsetScale.x);
-        	scale.y = Mathf.Max(scale.y, scaleYMin);
+            // Adjust y scale by x distance from core
+            scale.y *= 1 - (Mathf.Abs(offset.x) / sphereOffsetScale.x);                // h
+            scale.y = Mathf.Max(scale.y, scaleYMin);
 
-        	spTrans.localScale = scale;
+            spTrans.localScale = scale;                                                // i
+
         }
     }
 
     // Update is called once per frame
     void Update()
     {
-       // if (Input.GetKeyDown(KeyCode.Space)) {
-       // 	Restart();
+        //if (Input.GetKeyDown(KeyCode.Space))
+       // {                                         // j
+       //     Restart();
        // }
     }
 
-    void Restart() {
-    	// Clear out old spheres 
-    	foreach (GameObject sp in spheres) {
-    		Destroy(sp);
-    	}
+    void Restart()
+    {                                                                   // k
+        // Clear out old spheres
+        foreach (GameObject sp in spheres)
+        {
+            Destroy(sp);
+        }
 
-    	Start();
+        Start();
     }
 }
--- Assets/02-Mission Demolition/Scripts/CloudCrafter.cs
+++ Assets/02-Mission Demolition/Scripts/CloudCrafter.cs
@@ -1,86 +1,70 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using System.Collections;
 
 public class CloudCrafter : MonoBehaviour
 {
-	[Header("Set in Inspector")]
-	public int 	numClouds = 40; // the number of clouds to make
-	public GameObject 	cloudPrefab; // the prefab for the clouds
-	public Vector3 	cloudPosMin = new Vector3(-50, -5, 10);
-	public Vector3 	cloudPosMax = new Vector3(150, 100, 10);
-	public float 	cloudScaleMin = 1; // min scale for the clouds
-	public float 	cloudScaleMax = 3; // max scale for the clouds
-	public float 	cloudSpeedMult = 0.5f; // adjusts speed of clouds
+    [Header("Set in Inspector")]
+    public int numClouds = 40;         // The # of clouds to make
+    public GameObject cloudPrefab;            // The prefab for the clouds
+    public Vector3 cloudPosMin = new Vector3(-50, -5, 10);
+    public Vector3 cloudPosMax = new Vector3(150, 100, 10);
+    public float cloudScaleMin = 1;     // Min scale of each cloud
+    public float cloudScaleMax = 3;     // Max scale of each cloud
+    public float cloudSpeedMult = 0.5f; // Adjusts speed of clouds
 
-	private GameObject[] 	cloudInstances;
+    private GameObject[] cloudInstances;
 
-	void Awake() {
-		// make an array large enough to hold all the Cloud instances
-		cloudInstances = new GameObject[numClouds];
-
-		// find the CloudAnchor parent GameObject
-		GameObject anchor = GameObject.Find("CloudAnchor");
-
-		// iterate through and make clouds
-		GameObject cloud;
-		for (int i = 0; i < numClouds; i++) {
-			// make an instance of cloudPrefab
-			cloud = Instantiate<GameObject>(cloudPrefab);
-
-			// position of cloud
-			Vector3 cPos = Vector3.zero;
-			cPos.x = Random.Range(cloudPosMin.x, cloudPosMax.x);
-			cPos.y = Random.Range(cloudPosMin.y, cloudPosMax.y);
-
-			// scale cloud
-			float scaleU = Random.value;
-			float scaleVal = Mathf.Lerp(cloudScaleMin, cloudScaleMax, scaleU);
-
-			// smaller clouds (with smaller scaleU) should be near the ground
-			cPos.y = Mathf.Lerp(cloudPosMin.y, cPos.y, scaleU);
-
-			// smaller clouds should be farther away
-			cPos.z = 100 -90*scaleU;
-
-			// apply these transforms to the cloud 
-			cloud.transform.position = cPos;
-			cloud.transform.localScale = Vector3.one * scaleVal;
-
-			// make a cloud a child of the anchor
-			cloud.transform.SetParent(anchor.transform);
-
-			// add the cloud to cloudInstances
-			cloudInstances[i] = cloud;
-		}
-	}
-
-    // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
+        // Make an array large enough to hold all the Cloud_ instances
+        cloudInstances = new GameObject[numClouds];
+        // Find the CloudAnchor parent GameObject
+        GameObject anchor = GameObject.Find("CloudAnchor");
+        // Iterate through and make Cloud_s
+        GameObject cloud;
+        for (int i = 0; i < numClouds; i++)
+        {
+            // Make an instance of cloudPrefab
+            cloud = Instantiate<GameObject>(cloudPrefab);
+            // Position cloud
+            Vector3 cPos = Vector3.zero;
+            cPos.x = Random.Range(cloudPosMin.x, cloudPosMax.x);
+            cPos.y = Random.Range(cloudPosMin.y, cloudPosMax.y);
+            // Scale cloud
+            float scaleU = Random.value;
+            float scaleVal = Mathf.Lerp(cloudScaleMin, cloudScaleMax, scaleU);
+            // Smaller clouds (with smaller scaleU) should be nearer the ground
+            cPos.y = Mathf.Lerp(cloudPosMin.y, cPos.y, scaleU);
+            // Smaller clouds should be further away
+            cPos.z = 100 - 90 * scaleU;
+            // Apply these transforms to the cloud
+            cloud.transform.position = cPos;
+            cloud.transform.localScale = Vector3.one * scaleVal;
+            // Make cloud a child of the anchor
+            cloud.transform.SetParent(anchor.transform);
+            // Add the cloud to cloudInstances
+            cloudInstances[i] = cloud;
+        }
     }
 
-    // Update is called once per frame
     void Update()
     {
-        // iterate each over cloud that was created 
-        foreach (GameObject cloud in cloudInstances) {
-        	// get the cloud scale and position
-        	float scaleVal = cloud.transform.localScale.x;
-        	Vector3 cPos = cloud.transform.position;
-
-        	// move larger clouds faster
-        	cPos.x -= scaleVal * Time.deltaTime * cloudSpeedMult;
-
-        	// if a cloud has moved too far on the left
-        	if (cPos.x <= cloudPosMin.x) {
-        		// move it to the far right 
-        		cPos.x = cloudPosMax.x;
-        	}
-
-        	// apply the new position to cloud
-        	cloud.transform.position = cPos;
+        // Iterate over each cloud that was created
+        foreach (GameObject cloud in cloudInstances)
+        {
+            // Get the cloud scale and position
+            float scaleVal = cloud.transform.localScale.x;
+            Vector3 cPos = cloud.transform.position;
+            // Move larger clouds faster
+            cPos.x -= scaleVal * Time.deltaTime * cloudSpeedMult;
+            // If a cloud has moved too far to the left...
+            if (cPos.x <= cloudPosMin.x)
+            {
+                // Move it to the far right
+                cPos.x = cloudPosMax.x;
+            }
+            // Apply the new position to cloud
+            cloud.transform.position = cPos;
         }
     }
 }
